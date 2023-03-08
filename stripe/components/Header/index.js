49ceaac9158bb058bf5b4b9243d49bf8171e706d
silva05 <71233532+silva05@users.noreply.github.com>
2023-03-08 13:31:11 -0500

    Add files via upload

---
+++ stripe/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Box, Grid, Typography, Button} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+
+const WhiteTextTypography = withStyles({
+  root: {
+    color: "#FFFFFF"
+  }
+})(Typography);
+
+export default (props) => ( 
+    <Box py={10} bgcolor="secondary.main" color="white">
+        <Grid container justify="center">
+            <Grid item xs={10}>
+                <Box display="flex" justifyContent="space-between">
+                    <WhiteTextTypography variant="h4" font-color="white"> WORKFAST </WhiteTextTypography>
+                    <Button variant="contained" color="primary" disableElevation> CREAR TRABAJO </Button>
+                    <Button variant="contained" color="primary" disableElevation> COMPRAR SUSCRIPCION </Button>
+                </Box> 
+            </Grid>
+        </Grid>
+    </Box>
+);
